bf271eb8dbdd4adf7b5abf3ab2101a01ed99d323
Coachonko <info@coachonko.com>
2023-11-29 08:48:12 +0100

    Add verify test conditions

--- src/lexical_uuid_test.v
+++ src/lexical_uuid_test.v
@@ -31,29 +31,34 @@ fn test_v3() {
 fn test_verify() {
 	uuid_v4 := rand.uuid_v4()
 	verify(uuid_v4) or {
-		println(err)
 		assert false
 		return
 	}
 
+	stripped_v4 := uuid_v4.replace('-', '')
+	verify(stripped_v4) or {
+		assert false
+		return
+	}
+
+	invalid_v4_longer := uuid_v4 + '0'
+	verify(stripped_v4) or { assert true }
+
 	mut g := new_generator()
 	luuid_v1 := g.v1() or { panic(err) }
 	verify(luuid_v1) or {
-		println(err)
 		assert false
 		return
 	}
 
 	luuid_v2 := g.v2() or { panic(err) }
 	verify(luuid_v2) or {
-		println(err)
 		assert false
 		return
 	}
 
 	luuid_v3 := v3() or { panic(err) }
 	verify(luuid_v3) or {
-		println(err)
 		assert false
 		return
 	}
